7d197bf708caf97984a1ded1e4d8bf68f54f9259
shehap62 <shehapkhalil62@gmail.com>
2025-07-05 19:02:14 +0300

    edit

--- test_workflow.py
+++ test_workflow.py
@@ -10,7 +10,7 @@ import json
 import time
 
 # API base URL
-BASE_URL = " http://127.0.0.1:5000"
+BASE_URL = " https://nutrifit-agents-api.jollymoss-483cf973.uaenorth.azurecontainerapps.io"
 
 def test_workflow_status():
     """Test workflow status endpoint"""
